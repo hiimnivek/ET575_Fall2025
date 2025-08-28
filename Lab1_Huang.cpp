104de22faf8dba683cffaf0d2f8e191c65e47d87
hiimnivek <kevinhuang9908@gmail.com>
2025-08-28 02:08:17 +0000

    done

--- Lab1_Huang.cpp
+++ Lab1_Huang.cpp
@@ -15,12 +15,12 @@ int main()
     //variables = temporary storage of data
     //declare a variables first
     string username = "peterpan123";
-    cout<<"\nWelcome to C++ \t\t" <<username<<endl;
+    cout <<"\nWelcome to C++ \t\t" <<username<<endl;
     
     cout << " ------- Example 2: cin data -------"<<endl;
-    cout<<"Enter a new username" <<endl;
+    cout <<"Enter a new username" <<endl;
     cin >> username;
-    cout<<"New user is "<<username<<endl;
+    cout <<"New user is "<<username<<endl<<endl;
     
     cout << " ------- Example 3: data type -------"<<endl;
     //declare variable
@@ -29,16 +29,34 @@ int main()
     int number = 8;
 
     cout <<"Given symbol = " <<sym<<endl;
-    cout <<"Given disatance = " <<distance<<endl;
-    cout <<"Given number = " <<number<<endl;
+    cout <<"Given distance = " <<distance<<endl;
+    cout <<"Given number = " <<number<<endl<<endl;
 
     cout << "------ Exercise ------"<<endl;
     //declare variables (4 lines)
+    string country;
+    char gender;
+    int zipcode;
+    float rate;
 
+    // collect data
+    cout <<"Enter a country you want to visit: ";
+    cin >> country;
 
-    // collect data (4 values)
+    cout <<"Enter your gender(f for female, m for male, and o for others): ";
+    cin >> gender;
 
-    //print result (values)
+    cout <<"Enter your zipcode: ";
+    cin >> zipcode;
+
+    cout <<"Enter the sales tax for NY: ";
+    cin >> rate;
     
+    //print result
+    cout <<"Entered country: " <<country<<endl;
+    cout <<"Selected gender: " <<gender<<endl;
+    cout <<"Zip code:\t " <<zipcode<<endl;
+    cout <<"Tax rate:\t " <<rate<<endl;
+
     return 0;
 }
