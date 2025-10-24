bcaa103655bb5e4eabf9b0507fd5c624e6a5857c
hiimnivek <kevinhuang9908@gmail.com>
2025-10-24 19:13:55 +0000

    Lab 12 class examples

---
+++ Lab12_prefunction/Lab12_Huang.cpp
@@ -0,0 +1,35 @@
+/*
+Kevin Huang
+Oct 24th, 2025
+Lab 12, scooping, and predefined functions
+*/
+
+#include<iostream>
+
+//global variable - has to be before lab12_functions 
+int num = 8;
+
+#include "Lab12_functions.cpp"
+using namespace std;
+
+int main(){
+    cout<<"\n----- Example 1: local variables ------"<<endl;
+    int sum = add(3,7) + add(1,6); //sum is local variable in the main() function
+    cout<<"The total sum in main() is \t"<<sum<<endl;
+ 
+    cout<<"\n----- Example 2: global variables ------"<<endl;
+    cout<<"num original value = \t"<<num<<endl;
+    doSomething();
+    num = 5;
+    cout<<"num in main function = \t"<<num<<endl;
+ 
+    cout<<"\n----- Example 3: hypotenuse ------"<<endl;
+    float s1 = 2;
+    float s2 = 3;
+    float h = hyp(s1,s2);
+    printhyp(s1,s2,h);
+
+    cout<<"\n----- Exercise ------"<<endl;
+    
+    return 0;
+}
---
+++ Lab12_prefunction/Lab12_functions.cpp
@@ -0,0 +1,36 @@
+/*
+Kevin Huang
+Oct 24th, 2025
+Lab 12, scooping, and predefined functions
+*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+//example 1:function that calculates and returns the sum of two numbers
+
+int add(int num1,int num2){
+    int sum = num1 + num2;  //sum is a local variable in function add()
+    cout<<"The sum in function add() is \t"<<sum<<endl;
+    return sum;
+}
+
+//example 2:
+void doSomething(){
+    //global variable
+    num += 20;
+    cout<<"num in doSomething function: "<<num<<endl;
+}
+
+//example 3: c++ predefine functions
+//function passes two arguments and returns the hypotenuse of a right triangle.
+float hyp(float side1, float side2){
+    return sqrt(pow(side1, 2)+pow(side2, 2));
+
+}
+
+//function to print result
+void printhyp(float s1, float s2, float h){
+    cout<<"The hypotenuse of side "<<s1<<" and "<<s2<<" is "<<h<<endl;
+}
